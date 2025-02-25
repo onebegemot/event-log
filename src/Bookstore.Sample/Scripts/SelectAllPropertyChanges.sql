cbd547273e5ce0f0d776f79b76c8885867e2f9f8
Alex Holenko <cat-begemot@live.com>
2025-02-25 15:08:00 +0000

    Fix SQL query

--- src/Bookstore.Sample/Scripts/SelectAllPropertyChanges.sql
+++ src/Bookstore.Sample/Scripts/SelectAllPropertyChanges.sql
@@ -10,7 +10,7 @@
     'String' as Type
 from StringPropertyLog
          left join EntityLog on EntityLog.Id = StringPropertyLog.EntityLogEntryId
-         left join EventLog on EventLog.Id = EntityLog.Id
+         left join EventLog on EventLog.Id = EntityLog.EventLogEntryId
          left join EventTypeDescriptions ON EventLog.EventType = EventTypeDescriptions.EnumId
          left join EntityTypeDescriptions ON EntityLog.EntityType = EntityTypeDescriptions.EnumId
          left join PropertyTypeDescriptions ON StringPropertyLog.PropertyType = PropertyTypeDescriptions.EnumId
@@ -27,7 +27,7 @@ select
     'DateTime' as Type
 from DateTimePropertyLog
          left join EntityLog on EntityLog.Id = DateTimePropertyLog.EntityLogEntryId
-         left join EventLog on EventLog.Id = EntityLog.Id
+         left join EventLog on EventLog.Id = EntityLog.EventLogEntryId
          left join EventTypeDescriptions ON EventLog.EventType = EventTypeDescriptions.EnumId
          left join EntityTypeDescriptions ON EntityLog.EntityType = EntityTypeDescriptions.EnumId
          left join PropertyTypeDescriptions ON DateTimePropertyLog.PropertyType = PropertyTypeDescriptions.EnumId
@@ -44,7 +44,7 @@ select
     'Bool' as Type
 from BoolPropertyLog
          left join EntityLog on EntityLog.Id = BoolPropertyLog.EntityLogEntryId
-         left join EventLog on EventLog.Id = EntityLog.Id
+         left join EventLog on EventLog.Id = EntityLog.EventLogEntryId
          left join EventTypeDescriptions ON EventLog.EventType = EventTypeDescriptions.EnumId
          left join EntityTypeDescriptions ON EntityLog.EntityType = EntityTypeDescriptions.EnumId
          left join PropertyTypeDescriptions ON BoolPropertyLog.PropertyType = PropertyTypeDescriptions.EnumId
@@ -61,7 +61,7 @@ select
     'Int32' as Type
 from Int32PropertyLog
          left join EntityLog on EntityLog.Id = Int32PropertyLog.EntityLogEntryId
-         left join EventLog on EventLog.Id = EntityLog.Id
+         left join EventLog on EventLog.Id = EntityLog.EventLogEntryId
          left join EventTypeDescriptions ON EventLog.EventType = EventTypeDescriptions.EnumId
          left join EntityTypeDescriptions ON EntityLog.EntityType = EntityTypeDescriptions.EnumId
          left join PropertyTypeDescriptions ON Int32PropertyLog.PropertyType = PropertyTypeDescriptions.EnumId
@@ -78,7 +78,7 @@ select
     'Double' as Type
 from DoublePropertyLog
          left join EntityLog on EntityLog.Id = DoublePropertyLog.EntityLogEntryId
-         left join EventLog on EventLog.Id = EntityLog.Id
+         left join EventLog on EventLog.Id = EntityLog.EventLogEntryId
          left join EventTypeDescriptions ON EventLog.EventType = EventTypeDescriptions.EnumId
          left join EntityTypeDescriptions ON EntityLog.EntityType = EntityTypeDescriptions.EnumId
          left join PropertyTypeDescriptions ON DoublePropertyLog.PropertyType = PropertyTypeDescriptions.EnumId
@@ -95,7 +95,7 @@ select
     'Decimal' as Type
 from DecimalPropertyLog
          left join EntityLog on EntityLog.Id = DecimalPropertyLog.EntityLogEntryId
-         left join EventLog on EventLog.Id = EntityLog.Id
+         left join EventLog on EventLog.Id = EntityLog.EventLogEntryId
          left join EventTypeDescriptions ON EventLog.EventType = EventTypeDescriptions.EnumId
          left join EntityTypeDescriptions ON EntityLog.EntityType = EntityTypeDescriptions.EnumId
          left join PropertyTypeDescriptions ON DecimalPropertyLog.PropertyType = PropertyTypeDescriptions.EnumId
